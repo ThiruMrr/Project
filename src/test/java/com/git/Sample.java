2a7b992eba0a39e9f825de4f1fd59562f0654d08
ThiruMrr <thirumalai1305@gmail.com>
2025-02-27 22:45:45 +0530

    First Time Push

---
+++ src/test/java/com/git/Sample.java
@@ -0,0 +1,22 @@
+package com.git;
+
+import java.lang.reflect.Method;
+
+public class Sample {
+	private void ind() {
+		
+	}
+	private void aus() {
+
+	}
+	private void eng() {
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
