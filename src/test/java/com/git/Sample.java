7450333ac42eb7b81d02f4dacb44ecb4272c9419
ThiruMrr <thirumalai1305@gmail.com>
2025-02-27 23:23:29 +0530

    Raji Add a methods

--- src/test/java/com/git/Sample.java
+++ src/test/java/com/git/Sample.java
@@ -1,5 +1,6 @@
 package com.git;
 
+import java.lang.classfile.Signature.TypeArg.Bounded.WildcardIndicator;
 import java.lang.reflect.Method;
 
 public class Sample {
@@ -12,10 +13,20 @@ public class Sample {
 	private void eng() {
 
 	}
-	
-	
+	private static void nz() {
+		System.out.println("Newzland");
+	}
+	private static void sl() {
+		System.out.println("Srilanka");
+	}
+	private static void wi() {
+		System.out.println("Newzland");
+	}
 	
 	public static void main(String[] args) {
+		nz();
+		sl();
+		wi();
 		
 	}
 
