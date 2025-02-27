b663b9732110fad1d031005af56548aa19c7db1e
ThiruMrr <thirumalai1305@gmail.com>
2025-02-27 23:07:14 +0530

    Thiru add a methods

--- src/test/java/com/git/Sample.java
+++ src/test/java/com/git/Sample.java
@@ -1,22 +1,31 @@
 package com.git;
 
-import java.lang.reflect.Method;
-
 public class Sample {
 	private void ind() {
-		
+		System.out.println("India");
 	}
 	private void aus() {
-
+		System.out.println("Australia");
 	}
 	private void eng() {
-
+		System.out.println("England");
 	}
-	
-	
-	
+	private void sa() {
+		System.out.println("South Affrica");	
+	}
+	private void pak() {
+		System.out.println("Pakisthan");
+	}
+	private void ban() {
+		System.out.println("Bangaladesh");
+	}	
 	public static void main(String[] args) {
-		
+		Sample sample= new Sample();
+		sample.ind();
+		sample.aus();
+		sample.pak();
+		sample.eng();
+		sample.ban();
+		sample.sa();
 	}
-
 }
